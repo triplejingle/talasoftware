b45d16c1b100a120fc85838e825e3a0dbd8b4004
Talasoftware <dtalahatu@trinitisolutions.nl>
2024-07-17 14:45:23 +0200

    removed login

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -98,14 +98,6 @@ function MobileNavBar() {
                 </a>
               ))}
             </div>
-            <div className="py-6">
-              <a
-                href="#"
-                className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-              >
-                Log in
-              </a>
-            </div>
           </div>
         </div>
       </DialogPanel>
