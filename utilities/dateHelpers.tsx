73a2d74e0bd2e94bd6277cf83de05a75115d5229
Talasoftware <dtalahatu@trinitisolutions.nl>
2024-07-17 13:05:56 +0200

    added type

--- utilities/dateHelpers.tsx
+++ utilities/dateHelpers.tsx
@@ -1,6 +1,6 @@
 import moment from "moment/moment";
 
-export function getYears(date){
+export function getYears(date: string){
   const start = moment(date);
   const remainder = 6- (start.month() % 12);
   return moment().diff(date, 'years') + " years and " + remainder +" months"
