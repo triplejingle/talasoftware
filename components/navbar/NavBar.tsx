f4c45c2f943f8b500297f570510b39957bb6ccab
Talasoftware <dtalahatu@trinitisolutions.nl>
2026-06-16 11:02:50 +0200

    added type to classNames function

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -7,7 +7,7 @@ const navigation = [
   { name: 'Contact', href: '/contact', current: false }
 ]
 
-function classNames(...classes) {
+function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(' ')
 }
 
